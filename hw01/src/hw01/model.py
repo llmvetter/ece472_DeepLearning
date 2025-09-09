76ebd17de4cbf0237ff8f6dfdee5501f65f9e37e
llmvetter <lennart.vetter@yahoo.de>
2025-09-09 11:36:28 -0400

    HW01 - Abstracts basis expansion into own module

--- hw01/src/hw01/model.py
+++ hw01/src/hw01/model.py
@@ -16,6 +16,42 @@ class LinearModel:
     sigmas: np.ndarray
 
 
+class BasisExpansion(nnx.Module):
+    def __init__(
+        self,
+        *,
+        key: jax.Array,
+        n_kernels: int = 5,
+    ) -> None:
+        key_mu, key_sigma = jax.random.split(key)
+        self.m = n_kernels
+        self.mu = nnx.Param(
+            jax.random.normal(
+                key_mu,
+                (self.m, 1),
+            ),
+        )
+        self.sigma = nnx.Param(
+            jax.random.uniform(
+                key_sigma,
+                (self.m, 1),
+                minval=0.01,
+                maxval=0.99,
+            ),
+        )
+
+    def __call__(self, x: jax.Array) -> jax.Array:
+        """
+        Does RBF Expansion, returns matrix of shape (len(x), len(m_kernels))
+        """
+        x_expanded = jnp.expand_dims(x, axis=1)
+        exponential = -jnp.square(x_expanded - self.mu.value) / jnp.square(
+            self.sigma.value
+        )
+        phi = jnp.exp(jnp.sum(exponential, axis=-1))
+        return phi
+
+
 class NNXLinearModel(nnx.Module):
     """A Flax NNX module for a nonlinear rbf model."""
 
@@ -26,17 +62,13 @@ class NNXLinearModel(nnx.Module):
         n_kernels: int = 5,
     ) -> None:
         key = rngs.params()
+        key_w, key_expansion = jax.random.split(key, 2)
         self.m = n_kernels
-        self.w = nnx.Param(jax.random.normal(key, (self.m,)))
+        self.w = nnx.Param(jax.random.normal(key_w, (self.m,)))
         self.b = nnx.Param(jnp.zeros((1,)))
-        self.mu = nnx.Param(jax.random.normal(key, (self.m, 1)))
-        self.sigma = nnx.Param(
-            jax.random.uniform(
-                key,
-                (self.m, 1),
-                minval=0.01,
-                maxval=0.99,
-            )
+        self.expansion_module = BasisExpansion(
+            key=key_expansion,
+            n_kernels=self.m,
         )
 
     def __call__(self, x: jax.Array) -> jax.Array:
@@ -46,11 +78,7 @@ class NNXLinearModel(nnx.Module):
             weight each phi value with weight and sum up the weighted phis
             add bias"""
 
-        x_expanded = jnp.expand_dims(x, axis=1)
-        exponential = -jnp.square(x_expanded - self.mu.value) / jnp.square(
-            self.sigma.value
-        )
-        phi = jnp.exp(jnp.sum(exponential, axis=-1))
+        phi = self.expansion_module(x)
         phi_sum = jnp.sum(phi * self.w.value, axis=1) + self.b.value
         return phi_sum
 
@@ -60,6 +88,6 @@ class NNXLinearModel(nnx.Module):
         return LinearModel(
             weights=np.array(self.w.value).reshape([self.m]),
             bias=np.array(self.b.value).squeeze(),
-            mus=np.array(self.mu.value).reshape([self.m]),
-            sigmas=np.array(self.sigma.value).reshape([self.m]),
+            mus=np.array(self.expansion_module.mu.value).reshape([self.m]),
+            sigmas=np.array(self.expansion_module.sigma.value).reshape([self.m]),
         )
