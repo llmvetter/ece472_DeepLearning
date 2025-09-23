1554facece7ca96d698d0f92b8b5228eae4331cd
llmvetter <lennart.vetter@yahoo.de>
2025-09-23 09:58:57 -0400

    HW03 - Makes use of nnx.sequential

--- hw03/src/hw03/model.py
+++ hw03/src/hw03/model.py
@@ -36,26 +36,22 @@ class Classifier(nnx.Module):
         rngs: nnx.Rngs,
     ) -> None:
         # add dynamic creation
+        assert len(layer_depths) == len(layer_kernel_sizes)
 
-        self.conv1 = nnx.Conv(1, 32, kernel_size=(3, 3), rngs=rngs)
-        self.batch_norm1 = nnx.BatchNorm(32, rngs=rngs)
-        self.dropout1 = nnx.Dropout(rate=0.025, rngs=rngs)
-        self.conv2 = nnx.Conv(32, 64, kernel_size=(3, 3), rngs=rngs)
-        self.batch_norm2 = nnx.BatchNorm(64, rngs=rngs)
-        self.avg_pool = partial(nnx.avg_pool, window_shape=(2, 2), strides=(2, 2))
+        self.conv = nnx.Sequential(
+            Conv2d(1, 32, kernel_size=(3, 3), rngs=rngs),
+            nnx.Dropout(rate=0.025, rngs=rngs),
+            partial(nnx.avg_pool, window_shape=(2, 2), strides=(2, 2)),
+            Conv2d(32, 64, kernel_size=(3, 3), rngs=rngs),
+            partial(nnx.avg_pool, window_shape=(2, 2), strides=(2, 2)),
+        )
         self.linear1 = nnx.Linear(3136, 256, rngs=rngs)
         self.dropout2 = nnx.Dropout(rate=0.025, rngs=rngs)
         self.linear2 = nnx.Linear(256, 10, rngs=rngs)
 
     def __call__(self, x):
-        x = self.avg_pool(nnx.relu(self.batch_norm1(self.dropout1(self.conv1(x)))))
-        x = self.avg_pool(nnx.relu(self.batch_norm2(self.conv2(x))))
+        x = self.conv(x)
         x = x.reshape(x.shape[0], -1)  # flatten
         x = nnx.relu(self.dropout2(self.linear1(x)))
         x = self.linear2(x)
         return x
-
-
-@nnx.scan(in_axes=(nnx.Carry, 0), out_axes=nnx.Carry)
-def convolve(carry, layer: Conv2d):
-    return layer(carry)
