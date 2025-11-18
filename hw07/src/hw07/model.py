c0fbecf58beef75371398afbb6faf2f2a8d88b91
llmvetter <lennart.vetter@yahoo.de>
2025-11-18 10:10:03 -0500

    HW07 - Implements AutoEncoder

--- hw07/src/hw07/model.py
+++ hw07/src/hw07/model.py
@@ -33,10 +33,11 @@ class MLP(nnx.Module):
         output_activation=nnx.identity,
         rngs: nnx.Rngs,
     ):
-        key_in, key_hidden, key_out = jax.random.split(rngs.params(), 3)
+        key_in, key_hidden1, key_hidden2, key_out = jax.random.split(rngs.params(), 4)
+        self.hidden_activation = hidden_activation
 
         # dynamic layer stack creation
-        @nnx.split_rngs(splits=layer_depth)
+        @nnx.split_rngs(splits=layer_depth - 1)
         @nnx.vmap(in_axes=(0))
         def create_layer(rngs):
             return Layer(
@@ -52,8 +53,15 @@ class MLP(nnx.Module):
             act=hidden_activation,
             rngs=nnx.Rngs(params=key_in),
         )
-        self.hidden_layers = create_layer(
-            rngs=nnx.Rngs(params=key_hidden),
+        self.hidden_stack = create_layer(
+            rngs=nnx.Rngs(params=key_hidden1),
+        )
+        # later replaced with AutoEncoder
+        self.ae = Layer(
+            din=layer_width,
+            dout=layer_width,
+            act=output_activation,
+            rngs=nnx.Rngs(params=key_hidden2),
         )
         self.out_layer = Layer(
             din=layer_width,
@@ -62,12 +70,15 @@ class MLP(nnx.Module):
             rngs=nnx.Rngs(params=key_out),
         )
 
-    def __call__(self, x):
+    def __call__(self, x, get_logits: bool = False):
         x = self.in_layer(x)
         x = hidden_forward(
             carry=x,
-            layer=self.hidden_layers,
+            layer=self.hidden_stack,
         )
+        x = self.ae(x)
+        if get_logits is True:
+            return x
         x = self.out_layer(x)
         return x
 
@@ -75,3 +86,32 @@ class MLP(nnx.Module):
 @nnx.scan(in_axes=(nnx.Carry, 0), out_axes=nnx.Carry)
 def hidden_forward(carry, layer: Layer):
     return layer(carry)
+
+
+class AutoEncoder(nnx.Module):
+    def __init__(
+        self,
+        d_mlp: int = 100,
+        d_enc: int = 5000,
+        *,
+        rngs: nnx.Rngs,
+    ):
+        key_enc, key_dec = jax.random.split(rngs.params(), 2)
+        initializer = jax.nn.initializers.he_uniform()
+        self.W_enc = nnx.Param(
+            initializer(key_enc, (d_mlp, d_enc)),
+        )
+        self.W_dec = nnx.Param(
+            initializer(key_dec, (d_enc, d_mlp)),
+        )
+        self.b_enc = nnx.Param(jnp.zeros((d_enc,)))
+        self.b_dec = nnx.Param(jnp.zeros((d_mlp,)))
+        self.act = nnx.relu
+        self.l1_coeff = 0.005
+
+    def __call__(self, x: jax.Array) -> jax.Array:
+        # implements original version, not updated one
+        x = x - self.b_dec
+        activations = self.act(x @ self.W_enc + self.b_enc)
+        x_rcst = activations @ self.W_dec + self.b_dec
+        return x_rcst
